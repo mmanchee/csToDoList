92d66e65470fdb2d72a3b78796b38784e6d6584b
Mike Manchee <mikemanchee@gmail.com>
2020-09-23 10:08:42 -0700

    add Program functionality to the ToDo list
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -8,7 +8,37 @@ namespace ToDoList {
   {
     public static void Main()
     {
+      string line;
       
+      for (int index = 0; index < 1; )
+      {
+        Console.WriteLine("Would you like to add an item to your list?(add/view/exit)");
+        
+        line = Console.ReadLine();
+        if (line == "exit")
+        {
+          index = 1;
+        }
+        else if (line == "add")
+        {
+          Console.WriteLine("Please enter a description.");
+          line = Console.ReadLine();
+          new Item(line);
+        }
+        else if (line == "view")
+        {
+          List<Item> results = Item.GetAll();
+          foreach (Item element in results)
+          {
+            Console.WriteLine(element.Description);
+          }
+        }
+        else
+        {
+          Console.WriteLine("Error command not recognized.");
+        }
+      }
     }
   }
 }
+
