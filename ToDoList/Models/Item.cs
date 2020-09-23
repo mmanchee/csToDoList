d8bc9267f6003fe853499034fd298d721ae5efc7
Mike Manchee <mikemanchee@gmail.com>
2020-09-23 09:27:49 -0700

    add Program to the ToDo list
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,15 +1,17 @@
+using System.Collections.Generic;
+using System;
+
 namespace ToDoList.Models
 {
   public class Item
   {
-    // We add set; to the line below.
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
-      _instances.Add(this); // New code.
+      _instances.Add(this);
     }
     public static List<Item> GetAll()
     {
---
+++ ToDoList/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList {
+
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
