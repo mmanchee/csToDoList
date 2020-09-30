54eafedad626be50e8f7489b43da4bfb7dc69281
Mike Manchee <mikemanchee@gmail.com>
2020-09-29 20:07:58 -0700

    add tests for Find and GetId

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,7 +6,7 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests : IDisposable
+  public class ItemTest : IDisposable
   {
 
     public void Dispose()
@@ -79,6 +79,36 @@ namespace ToDoList.Tests
 
       //Assert
       CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      int result = newItem.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      //Act
+      Item result = Item.Find(2);
+
+      //Assert
+      Assert.AreEqual(newItem2, result);
     }
   }
 }
