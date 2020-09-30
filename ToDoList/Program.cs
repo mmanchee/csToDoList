79f7a86943b5500fef99233cfd44085f4c71b9bc
Mike Manchee <mikemanchee@gmail.com>
2020-09-29 19:39:25 -0700

    add code to Program.cs and Startup.cs

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,44 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList {
-
+namespace ToDoList
+{
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      string line;
-      
-      for (int index = 0; index < 1; )
-      {
-        Console.WriteLine("Would you like to add an item to your list?(add/view/exit)");
-        
-        line = Console.ReadLine();
-        if (line == "exit")
-        {
-          index = 1;
-        }
-        else if (line == "add")
-        {
-          Console.WriteLine("Please enter a description.");
-          line = Console.ReadLine();
-          new Item(line);
-        }
-        else if (line == "view")
-        {
-          List<Item> results = Item.GetAll();
-          foreach (Item element in results)
-          {
-            Console.WriteLine(element.Description);
-          }
-        }
-        else
-        {
-          Console.WriteLine("Error command not recognized.");
-        }
-      }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
-
