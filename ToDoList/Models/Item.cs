7edc81a612a01885041deb1eb6530291ff8ab58f
Mike Manchee <mikemanchee@gmail.com>
2020-09-29 19:42:59 -0700

    add code to HomeController.cs and Item.cs and remove bin and obj folders

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System;
 
 namespace ToDoList.Models
 {
@@ -8,18 +7,21 @@ namespace ToDoList.Models
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
     }
+
     public static List<Item> GetAll()
     {
       return _instances;
     }
+
     public static void ClearAll()
     {
       _instances.Clear();
     }
+
   }
 }
