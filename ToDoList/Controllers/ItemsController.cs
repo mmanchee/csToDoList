31c1a5904dd057fc9cebb04b58bd65db35b89860
Mike Manchee <mikemanchee@gmail.com>
2020-10-01 08:15:29 -0700

    add the connection between Category and Item

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -39,5 +39,21 @@ namespace ToDoList.Controllers
       Item foundItem = Item.Find(id);
       return View(foundItem);
     }
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
+    {
+      Category category = Category.Find(categoryId);
+      return View(category);
+    }
   }
 }
