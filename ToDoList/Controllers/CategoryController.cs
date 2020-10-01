3255895929bd98c9c15b184675ea7e5a23fe2291
Mike Manchee <mikemanchee@gmail.com>
2020-10-01 08:05:02 -0700

    add Create and Category/New HTML

--- ToDoList/Controllers/CategoryController.cs
+++ ToDoList/Controllers/CategoryController.cs
@@ -14,6 +14,16 @@ namespace ToDoList.Controllers
       List<Category> allCategories = Category.GetAll();
       return View(allCategories);
     }
-
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/categories")]
+  public ActionResult Create(string categoryName)
+  {
+    Category newCategory = new Category(categoryName);
+    return RedirectToAction("Index");
+  }
   }
 }
