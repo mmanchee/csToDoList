725d25d23cfe1461a89408aa1cb06d28d8ace0cb
Mike Manchee <mikemanchee@gmail.com>
2020-10-04 18:00:02 -0700

    add Details to the project

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -31,5 +31,10 @@ namespace ToDoList.Controllers
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
   }
 }
